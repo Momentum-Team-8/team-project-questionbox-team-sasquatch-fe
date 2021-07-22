d170920cc9766292a011ff63dcc2b6a092b675a3
Greg Sronce <greg.sronce@gmail.com>
2021-07-22 09:56:55 -0500

    moved temp data to data.js and able to access it

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import { results } from "../Data.js";
+import React, { useState } from 'react';
+import { results } from '../Data.js';
 
 export const Questions = () => {
-  const [questions] = useState(results);
+  const [questions] = useState(results)
   return (
     <div>
       <h1>All questions</h1>
       {questions.map((ask, idx) => {
         return (
-          <div>
+          <div key={idx}>
             <h3>{ask.title}</h3>
             <p>{ask.body}</p>
             <h4> asked by: {ask.author}</h4>
           </div>
-        );
+        )
       })}
     </div>
-  );
+  )
 };
