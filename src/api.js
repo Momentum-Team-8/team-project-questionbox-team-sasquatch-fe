234080cd99d0cbc484bd8958def001a8bb4e6958
jeanetteobr <jeanette.obr@gmail.com>
2021-07-23 17:48:26 -0400

    Fix component render issue with react router.

--- src/api.js
+++ src/api.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 export function requestLogin (username, password) {
   // a real login request would go here but in the meantime, we'll fake it
   return fakeResponse(username, password)
 }
 
-export function getQuestions() {
+export function getQuestions () {
   return axios
-    .get("https://questionbox-sasmothbe.herokuapp.com/api/questions/")
-    .then((res) => res.data);
+    .get('https://questionbox-sasmothbe.herokuapp.com/api/questions/')
+    .then((res) => res.data)
 }
 
-export function getQuestionDetail(question) {
+export function getQuestionDetail (id) {
   return axios
-    .get(`https://questionbox-sasmothbe.herokuapp.com/api/questions/${question.id}/`)
-    .then((res) => res.data);
+    .get(`https://questionbox-sasmothbe.herokuapp.com/api/questions/${id}/`)
+    .then((res) => res.data)
 }
 
-function fakeResponse(username, password) {
+function fakeResponse (username, password) {
   return new Promise((resolve, reject) => {
     if (username && password) {
       resolve({ auth_token: 'faketoken12345678' })
