825994a868a581cd9f6f58a7185a9a3e1463eef3
sarahartsoe <sarahartsoe@gmail.com>
2021-07-23 12:53:02 -0400

    started building question detail page. need to finish

--- src/Data.js
+++ src/Data.js
@@ -1,30 +1,34 @@
 export const results = [
   {
-    title: "question title",
-    author: "logan",
-    body: "this is the full question and full description of my question okay.",
-    created_at: "07-21-2021",
-    answers: ["O'Reilly"],
+    id: '1',
+    title: 'question title',
+    author: 'logan',
+    body: 'this is the full question and full description of my question okay.',
+    created_at: '07-21-2021',
+    answers: ["O'Reilly"]
   },
   {
-    title: "question 2 in the list",
-    author: "greg",
-    body: "this is the full question and full description of my question okay.",
-    created_at: "07-21-2021",
-    answers: ["O'Reilly"],
+    id: '2',
+    title: 'question 2 in the list',
+    author: 'greg',
+    body: 'how do i even ask a question?.',
+    created_at: '07-21-2021',
+    answers: ["O'Reilly"]
   },
   {
-    title: "another question to see",
-    author: "sara",
-    body: "this is the full question and full description of my question okay.",
-    created_at: "07-21-2021",
-    answers: ["O'Reilly"],
+    id: '3',
+    title: 'another question to see',
+    author: 'sara',
+    body: 'stuff and stuff then stuff and more stuff and stuff and stuff.',
+    created_at: '07-21-2021',
+    answers: ["O'Reilly"]
   },
   {
-    title: "help me",
-    author: "mr nobody",
-    body: "this is the full question and full description of my question okay.",
-    created_at: "07-21-2021",
-    answers: ["O'Reilly"],
-  },
-];
+    id: '4',
+    title: 'help me',
+    author: 'frodo',
+    body: 'looking for directions to mordor',
+    created_at: '07-21-2021',
+    answers: ["O'Reilly"]
+  }
+]
